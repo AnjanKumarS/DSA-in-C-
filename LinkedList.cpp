47f53d7216df1d074058e09d13692c7b41cc9061
AnjanKumarS <anjankumars65432@gmail.com>
2024-12-30 22:46:13 +0530

    Linked List

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -27,23 +27,41 @@ void insertAtTail(node* &head, int val){
     temp->next = n;
 }
 
+void insertAtHead(node* &head, int val){
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
 void display(node* head){
     node *temp = head;
     while(temp!=NULL){
-        cout << temp->data << " ";
+        cout << temp->data << "->";
         temp = temp->next;
     }
     cout << endl;
 }
 
-int main(){
+bool search(node* head,int key){
+    node *temp = head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
 
+int main(){
     node* head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
     insertAtTail(head, 4);
+    insertAtHead(head, 0);
     display(head);
+    cout << search(head, 5) << endl;
 
     return 0;
 }
