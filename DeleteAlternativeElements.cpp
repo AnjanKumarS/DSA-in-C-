fbd293fb1768351c32bfed4ad970dfe664540e6c
AnjanKumarS <anjankumars65432@gmail.com>
2025-01-01 08:18:40 +0530

    Delete Alternative Elements

---
+++ DeleteAlternativeElements.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int val;
+    node *next;
+
+    node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head,int val){
+    node* temp = new node(val);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAttail(node* &head,int val){
+    node *new_node = new node(val);
+    node *temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+void display(node* head){
+    node *temp = head;
+    while(temp!=NULL){
+        cout << temp->val << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void deleteAlternative(node* &head){
+    node *curr_node = head;
+    while(curr_node!=NULL && curr_node->next!=NULL){
+        node *temp = curr_node->next;
+        curr_node->next = temp->next;
+        delete temp;
+        curr_node = curr_node->next;
+    }
+}
+
+int main(){
+    node *head = NULL;
+    insertAtHead(head, 2);
+    insertAtHead(head, 1);
+    insertAttail(head, 3);
+    insertAttail(head, 4);
+    insertAttail(head, 5);
+    insertAttail(head, 6);
+    display(head);
+    deleteAlternative(head);
+    display(head);
+}
